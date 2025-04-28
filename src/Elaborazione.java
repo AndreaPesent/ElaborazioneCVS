58519f80a3eeaa0a371d48f60bdcdaa45deb862e
pesenti.21136 <pesenti.andrea.studente67@itispaleocapa.it>
2025-04-28 13:01:06 +0200

    prima classe e lettura file

--- src/Elaborazione.java
+++ src/Elaborazione.java
@@ -1,5 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Elaborazione
 {
-    String next;
+    public void Lettura()
+    {
+        String next;
+        double media=0;
+        double percentuale=0;
+        double sommaPercent=0;
+        String regione="";
+        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\pesenti.21136\\Desktop\\Grado-diffusione-del-PC-nelle-imprese-con-meno-di-10-addetti.csv")))
+        {
+            next=".";
+            while((next= br.readLine())!=null)
+            {
+                String[]dati=next.split(";");
+                regione=dati[1];
+                percentuale=Double.parseDouble(dati[2]);
+                sommaPercent+=percentuale;
+            }
 
+        } catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
 }
